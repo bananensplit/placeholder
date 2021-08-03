b13859ac0d7ed089ac7fb5fabbea5aa9f7ed52a3
Jeremiasz Zrolka <jeremiasz.zrolka@gmail.com>
2021-08-03 12:47:37 +0200

    Finished everything

---
+++ script.js
@@ -0,0 +1,14 @@
+reload();
+
+function reload() {
+    console.log("asdasd");
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let data = JSON.parse(this.responseText);
+      document.getElementById("img").src = data.link;
+    }
+  };
+  xhttp.open("GET", "https://some-random-api.ml/img/koala", true);
+  xhttp.send();
+}
