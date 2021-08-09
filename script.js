c07a79aee3da4092abe56885489689b5d0334e64
Jeremiasz <jeremiasz.zrolka@gmail.com>
2021-08-09 23:09:26 +0200

    Added links to social media

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-reload();
+otherKoala();
 
-function reload() {
+function otherKoala() {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
