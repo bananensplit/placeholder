7dc51b2206ee616e127cb05fefc8beaa387c0821
Jeremiasz <jeremiasz.zrolka@gmail.com>
2022-09-27 22:17:23 +0200

    added confetti animation and URI parameters

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-document.getElementById("firstname").addEventListener("keyup", event_listener, false);
-document.getElementById("firstname").addEventListener("change", event_listener, false);
-document.getElementById("secondname").addEventListener("keyup", event_listener, false);
-document.getElementById("secondname").addEventListener("change", event_listener, false);
+window.addEventListener("load", loadNamesFromURI, false);
 
-function names_to_numbers(name1, name2) {
+document.getElementById("firstname").addEventListener("keyup", eventListener, false);
+document.getElementById("firstname").addEventListener("change", eventListener, false);
+document.getElementById("secondname").addEventListener("keyup", eventListener, false);
+document.getElementById("secondname").addEventListener("change", eventListener, false);
+
+const pairs = [     // pairs of names for which confetti will be displayed
+    ["me", "myself"],
+    ["i", "me"],
+]
+
+
+function loadNamesFromURI() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let name1 = urlParams.get("n1") || "";
+    let name2 = urlParams.get("n2") || "";
+    document.getElementById("firstname").value = name1;
+    document.getElementById("secondname").value = name2;
+ 
+    updateNames(name1, name2);
+}
+
+function namesToNumbers(name1, name2) {
     let erg = [];
     name1 = name1.toUpperCase();
     name2 = name2.toUpperCase();
@@ -24,27 +42,34 @@ function names_to_numbers(name1, name2) {
     return erg;
 }
 
-function calc_match(name1, name2) {
-    data = names_to_numbers(name1, name2);
+function calcMatch(name1, name2) {
+    data = namesToNumbers(name1, name2);
 
     while (data.length > 2) {
         let erg = [];
-        
         for (let i = 0; i < Math.floor(data.length / 2); i++) {
             erg.push(data[i] + data[data.length - 1 - i]);
         }
-
         if (data.length % 2 != 0) erg.push(data[Math.round((data.length - 1) / 2)]);
-
         data = erg;
     }
 
     return parseInt(data.join(""));
 }
 
-function event_listener(event) {
+function updateNames(name1, name2) {
+    let erg = Math.max(calcMatch(name1, name2), calcMatch(name2, name1));
+    document.getElementById("percent").innerHTML = name1 == "" || name2 == "" ? "--%" : erg + "%";
+
+    if (name1.toUpperCase() == name2.toUpperCase()) return;
+    if (pairs.some(el => el.includes(name1) && el.includes(name2))) {
+        startConfetti();
+        setTimeout(stopConfetti, 3000);    
+    }
+}
+
+function eventListener(event) {
     let name1 = document.getElementById("firstname").value;
     let name2 = document.getElementById("secondname").value;
-    let erg = Math.max(calc_match(name1, name2), calc_match(name2, name1));
-    document.getElementById("percent").innerHTML = name1 == "" || name2 == "" ? "--%" : erg + "%";
+    updateNames(name1, name2);
 }
